a1f2dcad715295c547aa7ed4f2f19e2e1e3529a2
papa-react <makiya112545@gmail.com>
2022-02-10 04:50:42 -1200

    add  moralis appId and serverUrl

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,13 @@ import type { AppProps } from 'next/app'
 import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return
+    ;<MoralisProvider
+        appId={process.env.MORALIS_APP_ID || ''}
+        serverUrl={process.env.MORALIS_SERVER_URL || ''}
+    >
+        <Component {...pageProps} />
+    </MoralisProvider>
 }
 
 export default MyApp
