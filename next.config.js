2ffa3e84166f8e46cf36f76dc0da5a75b218638e
papa-react <makiya112545@gmail.com>
2022-02-10 03:17:15 -1200

    add domain name

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        domains: [
+            'avatars.dicebear.com',
+            'links.papareact.com',
+            'avatars.githubusercontent.com',
+        ],
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        formats: ['image/avif', 'image/webp'],
+    },
 }
