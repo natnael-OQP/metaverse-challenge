70f7c495a1131837d7e93f865744b6a6d8be6dc2
papa-react <makiya112545@gmail.com>
2022-02-11 01:56:18 -1200

    logout when login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import Login from 'components/Login'
 import Head from 'next/head'
+import { useMoralis } from 'react-moralis'
+
 export default function Home() {
+    const { isAuthenticated, logout, user } = useMoralis()
+    if (!isAuthenticated) return <Login />
+
     return (
         <div className="  ">
             <Head>
                 <title>Create Next App</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Login />
+            <div>
+                <div>welcome</div>
+                <button onClick={logout}>Logout</button>
+            </div>
         </div>
     )
 }
