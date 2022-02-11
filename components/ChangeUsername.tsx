a0c5e2d92ae6256a614320199ff881f1c7688257
papa-react <makiya112545@gmail.com>
2022-02-11 04:42:50 -1200

    changeUsername components

---
+++ components/ChangeUsername.tsx
@@ -0,0 +1,29 @@
+import { useMoralis } from 'react-moralis'
+
+function ChangeUsername() {
+    const { setUserData, isUserUpdating, user } = useMoralis()
+
+    const setUsername = () => {
+        const username = prompt(
+            `Enter your new Username '(current: ${user.getUsername()})'`
+        )
+        if (!username) return
+        setUserData({
+            username,
+        })
+    }
+
+    return (
+        <div className="absolute top-3 right-3 text-sm md:text-base ">
+            <button
+                className="font-semibold hover:text-pink-700"
+                disabled={isUserUpdating}
+                onClick={setUsername}
+            >
+                Change your Username
+            </button>
+        </div>
+    )
+}
+
+export default ChangeUsername
