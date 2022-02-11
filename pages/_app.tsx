ad8578bf8755130407eaf51d8c0ae1631d1c7abf
papa-react <makiya112545@gmail.com>
2022-02-11 01:54:45 -1200

    wrap with MoralisProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,14 @@ import type { AppProps } from 'next/app'
 import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return
-    ;<MoralisProvider
-        appId={process.env.MORALIS_APP_ID || ''}
-        serverUrl={process.env.MORALIS_SERVER_URL || ''}
-    >
-        <Component {...pageProps} />
-    </MoralisProvider>
+    return (
+        <MoralisProvider
+            appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID || ''}
+            serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_URL || ''}
+        >
+            <Component {...pageProps} />
+        </MoralisProvider>
+    )
 }
 
 export default MyApp
