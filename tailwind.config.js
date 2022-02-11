b75c01c3db7320879a79178e63de653f4c28c772
papa-react <makiya112545@gmail.com>
2022-02-11 06:50:55 -1200

    extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                color1: '#64ffda',
+                color2: '#8892b0',
+                color3: '#ccd6f6',
+            },
+        },
+    },
+    plugins: [],
 }
